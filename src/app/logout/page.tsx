0136ed5b70df7e2f78ed521aa0d8d212c19efaca
Ryan Yusuf Kurnianto <ryusuf11@gmail.com>
2025-04-17 07:38:30 +0700

    fix: redirect logout page csr

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { LoadingSpinner } from "@/components/atoms/loading-spinner";
 import { removeCookie } from "@/lib/cookie";
 
 export default function LogoutPage() {
-	const router = useRouter();
-
 	useEffect(() => {
 		setTimeout(() => {
 			removeCookie("auth_token");
 			removeCookie("auth_id");
-			router.replace("/");
+			window.location.replace("/");
 		}, 1500);
-	}, [router]);
+	}, []);
 
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
